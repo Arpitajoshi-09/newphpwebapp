df8f21cdf273862f504e16e6d3226add73a48c5f
Arpitajoshi-09 <125870137+Arpitajoshi-09@users.noreply.github.com>
2023-10-29 00:45:57 -0700

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'php-db.coohjlbmmpuw.us-east-2.rds.amazonaws.com');
+define('DB_SERVER', 'php-database.coohjlbmmpuw.us-east-2.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'admin123');
 define('DB_DATABASE', 'php');
